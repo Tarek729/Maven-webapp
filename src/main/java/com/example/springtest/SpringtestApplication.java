9340bd15038c0671d6d5c439285920bc475c13e0
Tarek729 <80055327+Tarek729@users.noreply.github.com>
2025-03-05 13:52:26 +0300

    test fail

--- src/main/java/com/example/springtest/SpringtestApplication.java
+++ src/main/java/com/example/springtest/SpringtestApplication.java
@@ -15,5 +15,6 @@ public class SpringtestApplication {
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
       return String.format("Hello %s!", name);
+	  f
     }
 }
