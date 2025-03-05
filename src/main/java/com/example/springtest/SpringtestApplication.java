916b4ace19e4cea9b0e3be35183b623c477738fa
Tarek729 <80055327+Tarek729@users.noreply.github.com>
2025-03-05 17:48:34 +0300

    added db support

--- src/main/java/com/example/springtest/SpringtestApplication.java
+++ src/main/java/com/example/springtest/SpringtestApplication.java
@@ -1,20 +1,30 @@
 package com.example.springtest;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class SpringtestApplication {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     public static void main(String[] args) {
-      SpringApplication.run(SpringtestApplication.class, args);
+        SpringApplication.run(SpringtestApplication.class, args);
     }
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
-	  
+
+    @GetMapping("/customers")
+    public List<Map<String, Object>> getCustomers() {
+        String sql = "SELECT * FROM Persons";
+        return jdbcTemplate.queryForList(sql);
     }
 }
